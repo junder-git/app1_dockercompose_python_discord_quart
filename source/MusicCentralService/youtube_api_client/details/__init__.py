3aac5275d58865f3dcf2c5ab53d2b3fe334ef074
J <j@junder.uk>
2025-04-21 21:06:28 +0100

    okok

--- source/MusicCentralService/youtube_api_client/details/__init__.py
+++
@@ -1,5 +0,0 @@
-from ClientYoutube.details.video_details import get_video_details
-from ClientYoutube.details.playlist_details import get_playlist_details
-from ClientYoutube.details.playlist_videos import get_playlist_videos
-
-__all__ = ['get_video_details', 'get_playlist_details', 'get_playlist_videos']
--- source/clients/youtube_api_client/search/__init__.py
+++
@@ -1,5 +0,0 @@
-from ClientYoutube.search.videos import search_videos
-from ClientYoutube.search.playlists import search_playlists
-from ClientYoutube.search.artists import search_artists
-
-__all__ = ['search_videos', 'search_playlists', 'search_artists']
