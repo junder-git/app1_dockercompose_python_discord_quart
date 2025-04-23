e215cf539157078e28c4f714d4c99c7947804887
J <j@junder.uk>
2025-04-23 16:15:46 +0100

    generate_csrf_token

--- jbot/ServiceQuartWeb/validators/__init__.py
+++ jbot/ServiceQuartWeb/validators/__init__.py
@@ -4,7 +4,7 @@ Validators package for Quart Web Service
 from .validate_csrf import validate_csrf
 from .validate_add_to_queue import validate_add_to_queue, validate_add_multiple, validate_add_playlist
 from .validate_search import validate_search_params
-from .validate_ids import validate_guild_id, validate_channel_id
+from .validate_ids import validate_guild_id, validate_channel_id, generate_csrf_token
 
 __all__ = [
     'validate_csrf',
@@ -13,5 +13,6 @@ __all__ = [
     'validate_add_playlist',
     'validate_search_params',
     'validate_guild_id',
-    'validate_channel_id'
+    'validate_channel_id',
+    'generate_csrf_token'
 ]
