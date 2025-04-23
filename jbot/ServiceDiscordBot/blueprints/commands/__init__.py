a7ff34ab22eb003f8201f8111eb16a53a82a2aef
J <j@junder.uk>
2025-04-23 18:02:40 +0100

    init all methods in discordbotpy

--- jbot/ServiceDiscordBot/blueprints/commands/__init__.py
+++ jbot/ServiceDiscordBot/blueprints/commands/__init__.py
@@ -23,25 +23,38 @@ def register_commands(bot):
         bot: The Discord bot instance
     """
     # Music commands
-    from .play import play_command
-    from .join import join_command
-    from .leave import leave_command
-    from .pause import pause_command
+    from .play import play_command, play_next
+    from .join import join_command, get_voice_client, follow_to_voice_channel, check_same_voice_channel
+    from .leave import leave_command, disconnect_from_voice
+    from .pause import pause_command, toggle_playback
     from .resume import resume_command
-    from .skip import skip_command
-    from .stop import stop_command
-    from .queue import queue_command
-    from .shuffle import shuffle_command
-    from .search import search_command
+    from .skip import skip_command, skip_track
+    from .stop import stop_command, stop_playback
+    from .queue import queue_command, clear_command, shuffle_command, move_command, clear_queue, shuffle_queue, reorder_queue
+    from .search import search_command, add_to_queue
     
     # Register commands with the bot
     bot.add_command(play_command)
+    bot.add_command(play_next)
     bot.add_command(join_command)
+    bot.add_command(get_voice_client)
+    bot.add_command(follow_to_voice_channel)
+    bot.add_command(check_same_voice_channel)
     bot.add_command(leave_command)
+    bot.add_command(disconnect_from_voice)
     bot.add_command(pause_command)
+    bot.add_command(toggle_playback)
     bot.add_command(resume_command)
     bot.add_command(skip_command)
+    bot.add_command(skip_track)
     bot.add_command(stop_command)
+    bot.add_command(stop_playback)
     bot.add_command(queue_command)
+    bot.add_command(clear_command)
     bot.add_command(shuffle_command)
+    bot.add_command(move_command)
+    bot.add_command(clear_queue)
+    bot.add_command(shuffle_queue)
+    bot.add_command(reorder_queue)
     bot.add_command(search_command)
+    bot.add_command(add_to_queue)
